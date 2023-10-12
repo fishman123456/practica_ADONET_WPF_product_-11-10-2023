58702df45ad13a075f7429bfe0ea571ebf17a6c8
denis <106389581+fishman123456@users.noreply.github.com>
2023-10-12 14:41:32 +0700

    добавил кнопку и метод проверки подключения
     Db_con.PingConnection();
    не работает нифига

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlClient;
 using System.Data;
 using System.Windows;
+using System;
 
 namespace practica_ADONET_WPF_product__11_10_2023
 {
@@ -19,10 +20,17 @@ namespace practica_ADONET_WPF_product__11_10_2023
         // вспомогательный метод обновления данных
         private void fillData()
         {
-            // вытягивание данных из БД согласно запросу
-            dataSet = new DataSet();    // в этот DataSet будет выполняться запись результата вытягивания
-            dataAdapter.Fill(dataSet, "product_t");  // выгружаем данные из БД и сохраняем в DataSet
-            dataGrid.ItemsSource = dataSet.Tables["product_t"].DefaultView;
+            try
+            {
+                // вытягивание данных из БД согласно запросу
+                dataSet = new DataSet();    // в этот DataSet будет выполняться запись результата вытягивания
+                dataAdapter.Fill(dataSet, "product_t");  // выгружаем данные из БД и сохраняем в DataSet
+                dataGrid.ItemsSource = dataSet.Tables["product_t"].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         public MainWindow()
         {
@@ -32,18 +40,18 @@ namespace practica_ADONET_WPF_product__11_10_2023
             // подготовка объектов
             dataAdapter = new SqlDataAdapter(fillCmd, Db_Connect.GetDbConnection());
             SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);  // строка заполняет INSERT/UPDATE/DELETE-команды
-            // реализацией по умолчанию
+            //c реализацией по умолчанию
         }
 
-       
-        private string fillCmd = "SELECT * from product_t p,  supplier_t s, type_t t " +
-            "where  p.type_id_f = t.id_f and p.supplier_id_f = s.id_f;";
 
+        private string fillCmd = "SELECT * from product_t p";
+        //  ,supplier_t s, type_t t " +
+        //  "where  p.type_id_f = t.id_f and p.supplier_id_f = s.id_f;";
         private void fillBtn_Click(object sender, RoutedEventArgs e)
         {
             fillData();
         }
-
+        // обновляем таблицу в базе данных
         private void updateBtn_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -55,6 +63,21 @@ namespace practica_ADONET_WPF_product__11_10_2023
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+        // проверяем соединение к базе данных
+        private void testconbtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                db_connect Db_con = new db_connect();
+                Db_con.PingConnection();
+                MessageBox.Show("Подключено к базе");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не Подключено к базе" + ex.ToString());
+            }
+
         }
     }
 }
--- db_connect.cs
+++ db_connect.cs
@@ -1,21 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.Common;
+﻿using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace practica_ADONET_WPF_product__11_10_2023
 {
@@ -29,6 +13,9 @@ namespace practica_ADONET_WPF_product__11_10_2023
             // connection.Open(); - в отсоединенном режиме подключение открывает и закрывает DbDataAdapter
             return connection;
         }
-
+        public void PingConnection()
+        {
+            using (SqlConnection connection = GetDbConnection()) { }
+        }
     }
 }
